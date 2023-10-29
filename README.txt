42234a8c1645f30237b95e8bb45509e15b1e0b4a
vindeshwari_prasad <vinayguptasia53@gmail.com>
2023-10-29 14:15:11 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+
+This is eccomerce app (MERN Stack)
---
+++ frontend/src/components/FooterComponent.js
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const FooterComponent = () => {
+    return ( <
+        footer >
+        <
+        Container fluid >
+        <
+        Row className = "mt-5" >
+        <
+        Col className = "bg-dark text-white text-center py-5" >
+        Copyright & copy; Everything ShopS <
+        /Col> <
+        /Row> <
+        /Container> <
+        /footer>
+    );
+};
+
+export default FooterComponent;
